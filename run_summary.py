e14a8a63ab65b3df0b1f68c53f6dd677e0f70595
tesrak-stats <tesrak@hotmail.com>
2025-06-30 05:52:57 -0700

    Update run_summary.py

--- run_summary.py
+++ run_summary.py
@@ -605,7 +605,7 @@ st.write("---")  # Separator before continuing to Basic Statistics
 
     
     # Show basic stats
-    st.write("## Basic Statistics")
+st.write("## Basic Statistics")
     col1, col2, col3, col4 = st.columns(4)
     
     with col1:
