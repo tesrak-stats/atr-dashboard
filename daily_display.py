7dd302b995992eff68ee3bc58f25026d2659508b
tesrak-stats <tesrak@hotmail.com>
2025-07-21 06:43:53 -0700

    Update daily_display.py

--- daily_display.py
+++ daily_display.py
@@ -564,7 +564,8 @@ if 'expanded_view_pref' not in st.session_state:
 
 col1_ui = st.columns([3, 1])
 with col1_ui:
-    show_expanded_view = st.checkbox("🖥️ Show Full Matrix (All Times & Levels)")
+    show_expanded_view = st.checkbox("🖥️ Show Full Matrix (All Times & Levels)", 
+                                   value=st.session_state.expanded_view_pref)
                                    
 
     #st.success("✅ Session default updated!")
